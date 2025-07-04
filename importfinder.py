8332a25d0e2638067ce1c0a6897df97e0f6b81c4
Leyton McKinney <leyton.mckinney@gmail.com>
2025-07-03 22:05:04 -0400

    Added basic directory search, find import functionality

--- importfinder.py
+++ importfinder.py
@@ -3,10 +3,14 @@ import sys
 import pip
 
 def find_py_files(root_dir: str) -> list:
+    '''
+    Use os.walk() to locate files in a directory and subdirectories thereof with the extension .py
+    '''
     pt_files = []
     for dirpath, _, files in os.walk(root_dir):
         for file in files:
-            if file.split(".")[1] == "py":
+            split = os.path.splitext(file)
+            if split[1] == "py":
                 pt_files.append(os.path.join(dirpath, file))
     return pt_files
 
@@ -14,6 +18,7 @@ def find_py_dependencies(pt_files: list) -> list:
     imports = []
     if not pt_files:
         raise PythonFilesNotFound("importfinder was unable to locate any Python files")
+
     for pt_file in pt_files:
         try:
             with open(pt_file, "r", encoding = "utf-8") as f:
@@ -23,20 +28,32 @@ def find_py_dependencies(pt_files: list) -> list:
                     line = line.strip("")
                     if len(line) > 7 and line[0:7] == "import ":
                         imp = line.split(" ")[1]
-                        imports.append(imp)
+                        imports.append(imp.strip())
 
         except FileNotFoundError:
-            print(f"{pt_file} was located by os.walk(), but could not be opened by the import searcher")
+            print(
+                f"{pt_file} was located by os.walk()," +
+                " but could not be opened by the import searcher"
+            )
+    return list(set(imports))
 
 class PythonFilesNotFound(Exception):
+    '''
+    PythonFilesNotFound is raised when a directory does not contain any .py files.
+    '''
     pass
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        root_dir = os.getcwd()
-    elif len(sys.argv) >= 2:
-        root_dir = sys.argv[1]
+    search_dir = os.getcwd()
+    if len(sys.argv) >= 2:
+        search_dir = sys.argv[1]
 
-    pt_files = find_py_files(root_dir)
-    find_py_dependencies(pt_files)
+    pt_files = find_py_files(search_dir)
+    try:
+        deps = find_py_dependencies(pt_files)
+    except PythonFilesNotFound as e:
+        raise PythonFilesNotFound(
+            f"importbuilder was unable to locate any .py files in directory {search_dir}"
+        ) from e
     
+    print(deps)
